813b2abfae025ee50b60cd18be740809e19265b1
MarcinK <marcin.kulbicki@gmail.com>
2023-02-28 19:26:15 +0100

    fix footer position at the bottom and vh100

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,18 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="oferta" element={<Offer />} />
-        <Route path="galeria" element={<Gallery />} />
-        <Route path="certyfikaty-i-uprawienia" element={<Certificates />} />
-        <Route path="kontakt" element={<Contact />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
+      <div className="components-container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="oferta" element={<Offer />} />
+          <Route path="galeria" element={<Gallery />} />
+          <Route path="certyfikaty-i-uprawienia" element={<Certificates />} />
+          <Route path="kontakt" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
