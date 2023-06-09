1d9a52329618459a858cd327761774705d7c968b
MarcinK <marcin.kulbicki@gmail.com>
2023-06-09 14:29:13 +0200

    hero layout parnters chagne

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,14 +8,12 @@ const Products = () => {
   const [mainServices, setMainServices] = useState(landingPageServices);
   return (
     <section className={` ${classes["section-services"]} section`}>
-      {/* <h3>Czym się zajmujemy</h3> */}
       <div className={`${classes["container-services"]}`}>
         {mainServices.map((element) => {
           const { id, title, img, services } = element;
           return (
             <div key={id} className={classes.service}>
               <h4>{title}</h4>
-              {/* <img src={img} alt={title} /> */}
               <ul>
                 {services.map((singleOffer, index) => {
                   return <li key={index}>{singleOffer}</li>;
